bbc8d126978266eedf0484e306f0734f43f1776a
AmiZyy <flamerius1@gmail.com>
2026-02-12 02:34:42 +0800

    Delete Motorcycle.java

--- Motorcycle.java
+++
@@ -1,8 +0,0 @@
-package domain.vehicles;
-import shared.Enums.VehicleType;
-
-public class Motorcycle extends Vehicle {
-    public Motorcycle(String licensePlate) {
-        super(licensePlate, VehicleType.MOTORCYCLE);
-    }
-}
