2cbe48d31c7bca7ffbd5e23d7e2d481feb8c7174
AmiZyy <flamerius1@gmail.com>
2026-02-12 02:35:01 +0800

    Delete Car.java

--- Car.java
+++
@@ -1,8 +0,0 @@
-package domain.vehicles;
-import shared.Enums.VehicleType;
-
-public class Car extends Vehicle {
-    public Car(String licensePlate) {
-        super(licensePlate, VehicleType.CAR);
-    }
-}
