07c32aa159c95a7196eb23795369ef67db45cc5c
AmiZyy <flamerius1@gmail.com>
2026-02-12 02:35:10 +0800

    Delete HandicappedVehicle.java

--- HandicappedVehicle.java
+++
@@ -1,8 +0,0 @@
-package domain.vehicles;
-import shared.Enums.VehicleType;
-
-public class HandicappedVehicle extends Vehicle {
-    public HandicappedVehicle(String licensePlate) {
-        super(licensePlate, VehicleType.HANDICAPPED);
-    }
-}
